7f3e0459c2e6df1293eec9f2f5f7e909df7bc841
AymenLaroussi <aymen.laroussi@live.fr>
2022-04-25 14:54:14 +0200

    Mise à jour

--- src/gui/commentaires/AddCommentaireController.java
+++ src/gui/commentaires/AddCommentaireController.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package gui.commentaires;
 
 import com.jfoenix.controls.JFXTextArea;
@@ -25,7 +30,7 @@ import javafx.scene.input.MouseEvent;
 /**
  * FXML Controller class
  *
- * @author Aymen Laroussi
+ * @author hocin
  */
 public class AddCommentaireController implements Initializable {
 
@@ -172,7 +177,6 @@ public class AddCommentaireController implements Initializable {
            
             
             produitID =Integer.parseInt(data1);
-
             System.out.println(produitID);
         }
         } catch (Exception e) {
