2a2a86bfb5a660991036c02e267031226e954cf9
AymenLaroussi <aymen.laroussi@live.fr>
2022-05-06 05:21:21 +0200

    Mise à jour boutique

--- src/boutique/Controller/ItemController.java
+++ src/boutique/Controller/ItemController.java
@@ -1,16 +1,18 @@
 package boutique.Controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import boutique.main.Main;
 import boutique.main.MyListener;
 import entities.Produit;
 import java.io.File;
 
+/**
+ *
+ * @author Aymen Laroussi
+ */
 public class ItemController {
     @FXML
     private Label nameLabel;
@@ -28,12 +30,27 @@ public class ItemController {
 
     private Produit produit;
     private MyListener myListener;
+    float total;
+    String total1;
+    float stq;
 
     public void setData(Produit produit, MyListener myListener) {
         this.produit = produit;
         this.myListener = myListener;
         nameLabel.setText(produit.getTitre());
-        priceLable.setText(Main.CURRENCY + produit.getPrix());
+        
+        float promo=produit.getPromo() ;
+        if (promo != 0){
+            total = (produit.getPromo()*produit.getPrix())/100;
+            if(total == (long) total)
+                total1=String.format("%d",(long)total);
+         else
+                total1 =String.format("%s",total);
+        }
+        else{
+            total=produit.getPrix(); 
+        }
+        priceLable.setText( total1+" TND");
              String A = "C:\\Pi\\public\\uploads\\"+produit.getImage();
                                           File F1 = new File(A);
                                            Image image = new Image(F1.toURI().toString());
