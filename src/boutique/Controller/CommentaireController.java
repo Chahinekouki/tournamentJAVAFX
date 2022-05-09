8a128667761229a97301dedd4533cf44d467a8fa
AymenLaroussi <aymen.laroussi@live.fr>
2022-05-09 02:55:29 +0200

    MAJ template + implémenter les services USER

--- src/boutique/Controller/CommentaireController.java
+++ src/boutique/Controller/CommentaireController.java
@@ -4,26 +4,44 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import boutique.main.Main;
 import boutique.main.MyListener;
+import entities.Produit;
 import entities.Commentaire;
+import java.io.File;
 import java.io.IOException;
+import java.math.RoundingMode;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import org.controlsfx.control.Rating;
 import utils.MyDB;
 
 /**
@@ -58,7 +76,7 @@ public class CommentaireController implements Initializable {
      public List<Commentaire> afficheProduit() throws SQLException {
         List<Commentaire> commentaires = new ArrayList<>();
          System.out.println(produit);
-        String req = "select distinct r.rating,r.user_code,r.entity_code,c.user_id,c.message,c.date,p.id,u.username,u.id,r.entity_code from commentaires c,rating r,user u ,produits p where r.user_code=u.id and r.entity_code = p.id and c.user_id = u.id and p.id ="+produit+" ORDER BY c.date desc";
+        String req = "select distinct r.rating,r.user_code,r.entity_code,c.user_id,c.message,c.date,p.id,u.username,u.id,r.entity_code from commentaires c,rating r,user u ,produits p where r.user_code=u.id and r.entity_code = p.id and p.id = "+produit+"";
         stm = connexion.createStatement();
         ResultSet rst = stm.executeQuery(req);
 
